6a708d4a3e2475ae3701e5f6a0b736dadf191ca7
Hussain Zaidi <syedhussainzaidi2002@gmail.com>
2025-05-18 23:48:39 -0400

    added run function in web search tool

--- src/tools/web_search_tool.py
+++ src/tools/web_search_tool.py
@@ -1,4 +1,6 @@
+import json
 from typing import Literal, Optional
+
 from src.utils.models_initializer import get_tavily_client, get_exa_client
 from pydantic import BaseModel, Field
 from langchain_core.callbacks import (
@@ -42,5 +44,29 @@ class WebSearch(BaseTool):
     name: str = "web_search_tool"
     description: str = (
         f"Conducts a web search using {chat_client} API for the given query. Keep the 'query' concise (under 300 characters)."
+        "The result contains a 'score' which is a relevance score of the search result to the query."
+        "content or highlights are snippets of the content from each result that are relevant to the query."
     )
     args_schema: Optional[ArgsSchema] = WebSearchToolSchema
+    
+    def _run(self, query: str, run_manager: CallbackManagerForToolRun) -> str:
+        """
+        Run the web search tool with the given query.
+        """
+        search_params = {}
+        if chat_client == "tavily":
+            search_params = web_search_params["tavily"]
+            client = get_tavily_client()
+            response = client.search(
+                query=query,
+                **search_params  # type: ignore
+            )
+        else:
+            search_params = web_search_params["exa"]
+            client = get_exa_client()
+            response = client.search(
+                query=query,
+                **search_params  # type: ignore
+            )
+        
+        return json.dumps(response) # convert the object to string
